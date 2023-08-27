a6f67c27a66519466dbb8f31c3d36f38ab0d1185
Dmitry <Dr.Sleep1995@gmail.com>
2023-08-27 11:12:02 +0300

    download books from tululu.org

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
+from pathlib import Path
+from random import randint
 
 
-url = "https://tululu.org/txt.php?id=32168"
+Path("books").mkdir(parents=True, exist_ok=True)
 
-response = requests.get(url)
-response.raise_for_status()
-
-filename = 'book.txt'
-with open(filename, 'w') as file:
-    file.write(response.text)
+for book_number in range(10):
+    url = f"https://tululu.org/txt.php?id={randint(10000,99999)}"
+    response = requests.get(url)
+    response.raise_for_status()
+    filename = f'book_{book_number+1}.txt'
+    with open('books/'+filename, 'w') as file:
+        file.write(response.text)
