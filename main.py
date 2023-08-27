a5af5d388e3ef327396e5d120904b03111115156
Dmitry <Dr.Sleep1995@gmail.com>
2023-08-27 11:30:12 +0300

    check_for_redirect

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ from pathlib import Path
 from random import randint
 
 
-Path("books").mkdir(parents=True, exist_ok=True)
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
 
-for book_number in range(10):
-    url = f"https://tululu.org/txt.php?id={randint(10000,99999)}"
-    response = requests.get(url)
-    response.raise_for_status()
-    filename = f'book_{book_number+1}.txt'
-    with open('books/'+filename, 'w') as file:
-        file.write(response.text)
+
+if __name__ == '__main__':
+    Path("books").mkdir(parents=True, exist_ok=True)
+
+    for book_number in range(10):
+        url = f"https://tululu.org/txt.php?id={book_number+1}"
+        response = requests.get(url)
+        response.raise_for_status()
+        try:
+            check_for_redirect(response)
+        except requests.HTTPError:
+            continue
+        filename = f'book_{book_number+1}.txt'
+        with open('books/'+filename, 'w') as file:
+            file.write(response.text)
