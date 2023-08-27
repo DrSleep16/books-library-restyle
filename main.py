a06487e95f409166eb9747daad278581c97613fd
Dmitry <Dr.Sleep1995@gmail.com>
2023-08-27 16:14:41 +0300

    download books with titles

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 import requests
 from pathlib import Path
-from random import randint
+from bs4 import BeautifulSoup
+import pathvalidate
 
 
 def check_for_redirect(response):
@@ -8,17 +10,44 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-if __name__ == '__main__':
-    Path("books").mkdir(parents=True, exist_ok=True)
+def get_book_title(book_number):
+    url = f"https://tululu.org/b{book_number}/"
+    response = requests.get(url)
+    response.raise_for_status()
 
+    soup = BeautifulSoup(response.text, 'lxml')
+    book_title = soup.find('h1')
+
+    if book_title:
+        return book_title.text.split('::')[0].strip()
+    else:
+        return "Unknown Title"
+
+
+def download_txt(url, book_number, folder='books/'):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    try:
+        check_for_redirect(response)
+    except requests.HTTPError:
+        return 'redirect'
+
+    book_title = get_book_title(book_number)
+
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    safe_filename = pathvalidate.sanitize_filename(f'{book_number}. {book_title}')
+    file_path = Path(folder) / (safe_filename + '.txt')
+
+    with open(file_path, 'w') as file:
+        file.write(response.text)
+
+    return str(file_path)
+
+
+
+if __name__ == '__main__':
     for book_number in range(10):
         url = f"https://tululu.org/txt.php?id={book_number+1}"
-        response = requests.get(url)
-        response.raise_for_status()
-        try:
-            check_for_redirect(response)
-        except requests.HTTPError:
-            continue
-        filename = f'book_{book_number+1}.txt'
-        with open('books/'+filename, 'w') as file:
-            file.write(response.text)
+        filepath = download_txt(url, str(book_number + 1), 'books/')
+        print(filepath)
