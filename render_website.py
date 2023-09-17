68716848928ef3b27fe93b0d44eeda38205db5ba
Dmitry <Dr.Sleep1995@gmail.com>
2023-09-17 16:17:01 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,4 +1,7 @@
 import json
+import os
+import itertools
+import more_itertools
 
 from jinja2 import Environment, FileSystemLoader
 from livereload import Server
@@ -7,14 +10,22 @@ from livereload import Server
 def rebuild():
     with open("media/book_parse.json", 'r', encoding="utf-8") as json_file:
         books = json.load(json_file)
-    env = Environment(loader=FileSystemLoader("."))
-    template = env.get_template("template.html")
-    render_html = template.render(books=books)
-    with open("index.html", 'w', encoding="utf-8") as html_file:
-        html_file.write(render_html)
+    page_size = 20
+    book_pages = list(more_itertools.chunked(books, page_size))
+    for page_num, page_books in enumerate(book_pages, start=1):
+        env = Environment(loader=FileSystemLoader("."))
+        template = env.get_template("template.html")
+        render_html = template.render(books=page_books)
+
+        page_filename = f"pages/index{page_num}.html"
+        with open(page_filename, 'w', encoding="utf-8") as html_file:
+            html_file.write(render_html)
 
 
-rebuild()
-server = Server()
-server.watch('template.html', rebuild)
-server.serve(root='.')
+if __name__ == '__main__':
+    if not os.path.exists("pages"):
+        os.makedirs("pages")
+    rebuild()
+    server = Server()
+    server.watch('template.html', rebuild)
+    server.serve(root='.')
